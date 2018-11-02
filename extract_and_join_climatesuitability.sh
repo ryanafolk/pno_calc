7831a3f5da9b6a571152d0da95412d87e2b1f73a
Ryan A. Folk <folk.41@buckeyemail.osu.edu>
2018-11-02 11:14:13 -0400

    Add files via upload

--- extract_and_join_climatesuitability.sh
+++ extract_and_join_climatesuitability.sh
@@ -1,6 +1,9 @@
 # PNO calculator script
 # Must have gdal tools in path
 
+modelpath=$1
+layerpath=$2
+
 mkdir cropped_environment
 mkdir model_dataframe
 mkdir joined_data
@@ -8,13 +11,13 @@ mkdir results
 
 #######
 # We start by generating joined dataframes of suitability and climate attributes
-for h in ./all_asc_narrowbuffer/*; do
+for h in ./${modelpath}/*; do
 i=$(echo ${h} | sed 's/.*\///g') # Model file name
 i=$(echo ${i} | sed 's/\..*//g') # Species name
 gdaltindex ${i}.shp ${h} # Shape file from model extent
 gdal_translate -of XYZ ${h} ./model_dataframe/${i}.xyz # Output in dataframe-like format
 
-for f in ./layers_tif/*.tif; do
+for f in ./${layerpath}/*.tif; do
 g=$(echo ${f} | sed 's/.*\///g') # Environmental data file name
 g=$(echo ${g} | sed 's/\..*//g') # Variable name
 echo ${f}
@@ -24,13 +27,15 @@ gdal_translate -of XYZ ./cropped_environment/${g}_${i}.tif ./cropped_environment
 
 # This command creates a key with awk comprising the first two fields of each XYZ file, inserted as a first column.
 # Then it sorts on this key and does a join on this field, printing the right fields, then deleting any missing data.
-join -j 1 -o 1.2,1.3,1.4,2.4 <(awk '{print $1"-"$2" "$0}' ./model_dataframe/${i}.xyz | sort -k1,1 ) <(awk '{print $1"-"$2" "$0}' ./cropped_environment/${g}_${i}.xyz | sort -k1,1 ) | sed '/-9999/d' > ./joined_data/${g}_${i}.xyz
+join -j 1 -o 1.2,1.3,1.4,2.4 <(awk '{print $1"-"$2" "$0}' ./model_dataframe/${i}.xyz | sort -S 25% -k1,1 ) <(awk '{print $1"-"$2" "$0}' ./cropped_environment/${g}_${i}.xyz | sort -S 25% -k1,1 ) | sed '/-9999/d' > ./joined_data/${g}_${i}.xyz
 
 done
 
 rm ${g}.shp ./cropped_environment/${g}_${i}.tif # Clean up
 rm ./model_dataframe/${i}.xyz ./cropped_environment/${g}_${i}.xyz
 done
+
+rm *.shp *.shx *.dbf
 
 ########
 # Now per variable we do the PNO calculation
@@ -42,13 +47,13 @@ rm maxima.tmp
 
 i=$(ls ./joined_data/*.xyz | wc -l) # Number of relevant files in directory
 j=1 # Progress counter
-for y in ./layers_tif/*.tif; do
+for y in ./${layerpath}/*.tif; do
 z=$(echo ${y} | sed 's/.*\///g') # Environmental data file name
 z=$(echo ${z} | sed 's/\..*//g') # Variable name
 echo ${z}
 
 for f in $(ls ./joined_data/${z}_*.xyz); do # Include underscore to avoid incorrect hits
-cut -d" " -f4 ${f} | sort -n | sed -n '1p;$p' > tmp.tmp # First line is min second line is max
+cut -d" " -f4 ${f} | sort -n -S 50% | sed -n '1p;$p' > tmp.tmp # First line is min second line is max
 head -1 tmp.tmp >> minima.tmp
 tail -1 tmp.tmp >> maxima.tmp
 # Progress
